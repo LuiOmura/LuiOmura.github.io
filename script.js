fbf45ef235b7a03f183d1c4a1f720ba6b94d0520
LuiOmura <56373287+LuiOmura@users.noreply.github.com>
2026-01-21 14:38:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // CONFIG
   const GOOGLE_FORM_ENDPOINT =
-    "https://docs.google.com/forms/d/1u8jdamQHqGgSchlyv9S0ZmoJr1-4wRc5Bb9VG4eqriw/formResponse";
+    "https://docs.google.com/forms/d/e/1u8jdamQHqGgSchlyv9S0ZmoJr1-4wRc5Bb9VG4eqriw/formResponse";
 
   const ENTRY = {
     fullName: "entry.500061655",
